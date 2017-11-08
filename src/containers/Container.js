76578c70e22d3e682aa8eb033b84148a5ed0659b
Tara <tara@nelsonwriting.com>
2017-11-08 12:43:25 -0500

    Set up basic web interface.

---
+++ src/containers/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Container = () => {
+
+    const containerStyle = {border: '#404041 solid 1px', padding: '200px'};
+        return (
+            <div style={containerStyle}>
+                <h1>User profiles go here</h1>
+            </div>
+        );
+}
+
+export default Container;
