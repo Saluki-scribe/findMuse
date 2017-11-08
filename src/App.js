f487a5976187f961a96d157f098f8716e7d9c590
Tara <tara@nelsonwriting.com>
2017-11-08 13:28:44 -0500

    Added router capabilities

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Container from './containers/Container';
 
-
-
 class App extends Component {
   render() {
     return (
